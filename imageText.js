8f3eb5cd891592931227278159f2735127a164d2
Mikal Stapnes <mstapnes@gmail.com>
2017-06-26 21:39:27 +0200

    Include several images, none of which work at the moment

---
+++ imageText.js
@@ -0,0 +1,8 @@
+var imageText = [
+    'Stapneset, 2016.',
+    'Utsikt til Moi, 2016.',
+    'Moskusender, 2014.',
+    'Flyfoto, 1954. Eier: Dalane Folkemuseum.',
+    'Utsikt fra Varberg.',
+    'Smistein, 2015.'
+];
