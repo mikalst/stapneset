2e53cf12b8b087cfb41ffa195d295cffae33c539
Mikal Stapnes <mstapnes@gmail.com>
2018-07-22 16:39:44 +0200

    Many changes.

--- imageText.js
+++ imageText.js
@@ -1,10 +1,10 @@
 var imageText = [
     'Stapneset, 2016.',
-    'Utsikt til Moi, 2016.',
+    'Utsikt til Mong, 2016.',
     'Moskusender, 2014.',
     'Familien Anders Sørenson Stapnes samlet foran bolighuset på gården på Stapnes, ca 1902. {Eier: Dalane Folkemuseum}',
     'Flyfoto, 1954. {Eier: Dalane Folkemuseum}',
     'Utsikt fra Varberg.',
     'Smistein, 2015.',
-    'Jakob Berentson Stapnes med familie står forran huset på gården på Stapnes, ca 1902. {Eier: Dalane Folkemuseum}'
+    'Jakob Berentson Stapnes med familie står foran huset på gården på Stapnes, ca 1902. {Eier: Dalane Folkemuseum}'
 ];
--- imageviewer.js
+++ imageviewer.js
@@ -1,3 +1,6 @@
+/*jslint browser: true*/
+/*global $, jQuery, alert, console*/
+
 var baseUrl = 'images/';
 var pictureIndex = 0;
 var pictures = [];
@@ -5,13 +8,14 @@ var pictures = [];
 $.getScript('imageText.js');
 
 function getFiles() {
+    "use strict";
     $.ajax(baseUrl).success(function (data) {
         pictures = [];
         
         $(data).find("a[href]").each(function () {
             var href = $(this).attr('href');
             if (href.indexOf('.jpg') > 0 || href.indexOf('.png') > 0 || href.indexOf('.jpeg') > 0) {
-	           pictures.push(href);
+	            pictures.push(href);
             }
         });
         
@@ -21,6 +25,7 @@ function getFiles() {
 }
 
 function changePicture(indexOffset) {
+    "use strict";
     pictureIndex += indexOffset;
     if (pictureIndex >= pictures.length) {
         pictureIndex = 0;
@@ -33,14 +38,3 @@ function changePicture(indexOffset) {
 }
 
 getFiles();
-    
-    /*
-	$(document).keydown(function(e){
-		var left = -1, right = 1;
-	    if (e.keyCode == 37) {
-	       changePicture(left); return false;
-	    } else if (e.keyCode == 39) {
-	    	changePicture(right); return false;
-	    }
-	});
-    */
